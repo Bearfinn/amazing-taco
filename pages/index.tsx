dc01809836d940e0eb818a95fdd0bd44b741f577
Kritsada Sunthornwutthikrai <kritsada.stw@gmail.com>
2021-12-07 00:13:37 +0700

    feat: Get total shing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import styles from "../styles/Home.module.css";
 import { rpc } from "../utils/wax";
 
 const Home: NextPage = () => {
-  const [address, setAddress] = useState("")
+  const [address, setAddress] = useState("");
   const [data, setData] = useState<any>(null);
-  const [extractors, setExtractors] = useState<any[]>([])
+  const [extractors, setExtractors] = useState<any[]>([]);
 
   const getData = async ({ address }: any) => {
     const { rows } = await rpc.get_table_rows({
@@ -19,9 +19,9 @@ const Home: NextPage = () => {
       // index_position: 1,
       upper_bound: "bananaminiox",
       lower_bound: "banabaminion",
-      limit: 10
+      limit: 10,
     });
-    
+
     return rows;
   };
 
@@ -32,32 +32,45 @@ const Home: NextPage = () => {
       scope: "g.taco",
       table: "configextr",
       // index_position: 1,
-      limit: 10
+      limit: 10,
     });
-    
+
     return rows;
   };
 
   const getExtractor = (key: number) => {
-    return extractors.find((extractor) => extractor.template_id === key)
+    return extractors.find((extractor) => extractor.template_id === key);
+  };
+
+  const calculateShing = (value: number) => {
+    return value / 28 * 10.08
+  }
+
+  const calculateTotalShingPerHour = (extractors: any[] = []) => {
+    let sum = 0
+    extractors.forEach((extractorInfo) => {
+      const extractor = getExtractor(extractorInfo.key)
+      sum += calculateShing(extractor.value) * extractorInfo.value
+    })
+    return sum
   }
 
   useEffect(() => {
     getExtractorConfig().then((response) => {
-      setExtractors(response)
-    })
-  }, [])
+      setExtractors(response);
+    });
+  }, []);
 
   const fetchData = async () => {
     if (address) {
       getData(address).then((response) => {
         setData(response[0]);
-      })
+      });
     }
-  }
+  };
 
   // useEffect(() => {
-  //   fetchData()
+  //   calculateTotalShingPerHour()
   // }, [address]);
 
   return (
@@ -73,35 +86,51 @@ const Home: NextPage = () => {
           Shingilator <span className="text-sm">Beta</span>
         </h1>
 
-        <input value={address} onChange={(e) => setAddress(e.target.value)}></input>
-        <button onClick={() => fetchData()}>Check</button>
-        <div>
-        {JSON.stringify(data)}
+        <input
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        ></input>
 
+        <div>
+          {data?.extractors && calculateTotalShingPerHour(data.extractors)}
+        </div>
+        <button className="text-gray-500" onClick={() => fetchData()}>Check</button>
+        <div>{JSON.stringify(data)}</div>
+        <div>{data?.account}</div>
+        <div className="flex gap-4">
+          {data &&
+            data.extractors.map(
+              (extractorInfo: { key: number; value: number }) => {
+                const extractor = getExtractor(extractorInfo.key);
+                return (
+                  <div
+                    className="shadow-md p-4 rounded-md w-48"
+                    key={extractorInfo.key}
+                  >
+                    <div className="flex items-baseline justify-between">
+                      <div className="text-lg">{extractor.label}</div>
+                      <div className="text-gray-500">
+                        {extractorInfo.value}x
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="pt-4">
+                      <div>{calculateShing(extractor.value) * extractorInfo.value} SHING</div>
+                      <div className="text-sm text-gray-500">{calculateShing(extractor.value)} SHING/hr each</div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
         </div>
         <div>
-          {data?.account}
+          {data &&
+            data.bonus.map((bonus: { key: number; value: number }) => {
+              <div>{bonus.value}</div>;
+            })}
         </div>
-        <div>
-          {data && data.extractors.map((extractor: { key: number, value: number }) => {
-            return <div key={extractor.key}>
-              {extractor.value}x {getExtractor(extractor.key).label} {getExtractor(extractor.key).value / 28 * 10.08}
-            </div>
-          })}
-        </div>
-        <div>
-          {data && data.bonus.map((bonus: { key: number, value: number }) => {
-            <div>
-              {bonus.value}
-            </div>
-          })}
-        </div>
-        <div>
-          Last claimed: { data && data.last_claim }
-        </div>
-        <div>
-          To claim: { data && data.to_claim }
-        </div>
+        <div>Last claimed: {data && data.last_claim}</div>
+        <div>To claim: {data && data.to_claim}</div>
       </main>
 
       <footer className={styles.footer}>
